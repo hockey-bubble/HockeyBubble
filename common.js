ea3125ccc5a624e199340b9da1d543de659938c4
AMorse03 <andrew.morse@ucc.on.ca>
2019-05-21 12:21:34 -0400

    Update common.js

--- common.js
+++ common.js
@@ -86,6 +86,9 @@
             
             for (var u in data) {
                 for (var t in data[u]) {
+                    if (team == data[u][t].team){
+                        
+                    
                     mytab = mytab + "<tr>";
                     var date = new Date(data[u][t].time);
                     var time = date.toDateString();
@@ -103,6 +106,7 @@
                         
                     } 
                     mytab = mytab + "</tr>";
+                    }
                 }   
             }
             
